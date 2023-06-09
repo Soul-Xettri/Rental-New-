4aa1699317df35612e96aad6e902127be675e241
soul-xettri <rakeshchhetri420@gmail.com>
2023-06-09 14:41:52 +0545

    small changes

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Authentication from "./components/Auth/Authentication";
-import { TopHeader } from "./components/Navbar/TopHeader";
+// import { TopHeader } from "./components/Navbar/TopHeader";
 import { ForgotPassword } from "./components/Auth/ForgotPassword";
 import { Error404 } from "./components/PageError/Error404";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
